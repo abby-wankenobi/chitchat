f3237e2fb01b986043fa783150ad7709939c7af8
Abigail Swarth <abigailswarth@gmail.com>
2020-02-08 18:47:36 -0500

    join page layout

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React, { Component, useState }  from 'react';
+import { Link } from 'react-router-dom';
+
+import './Chat.css';
+
+const Chat = () => {
+    return (
+        <h1>Chat</h1>
+    )
+}
+
+export default Chat;
